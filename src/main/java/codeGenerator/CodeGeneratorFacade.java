0be9ce6a9ee3583749c5a0190b9a10ea1ab469fe
Ali Rezaei <alirezaeim1380@gmail.com>
2023-12-11 13:55:58 +0330

    refac(codegenerator): Add code generator facade class

---
+++ src/main/java/codeGenerator/CodeGeneratorFacade.java
@@ -0,0 +1,15 @@
+public class CodeGeneratorFacade {
+    private CodeGenerator codeGenerator;
+
+    public CodeGeneratorFacade() {
+        codeGenerator = new CodeGenerator();
+    }
+
+    public void semanticFunction(int func, Token next) {
+        codeGenerator.semanticFunction(func, next);
+    }
+
+    public void printMemory() {
+        codeGenerator.printMemory();
+    }
+}
